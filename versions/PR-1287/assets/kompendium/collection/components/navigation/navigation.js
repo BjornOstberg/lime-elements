8d67cd62bbed01de50c660309d07beac4951604a
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-07-22 07:54:28 +0000

    chore(docs): create docs PR-1287

--- versions/PR-1287/assets/kompendium/collection/components/navigation/navigation.js
+++ versions/PR-1287/assets/kompendium/collection/components/navigation/navigation.js
@@ -1,16 +1,16 @@
-import { Component, h, Prop, State, getAssetPath, Element, } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 /**
  * @private
  */
 export class Navigation {
   constructor() {
     this.route = '';
+    this.displayNavPanel = false;
     this.toggleMenu = () => {
-      const panel = this.host.shadowRoot.querySelector('.nav-panel');
-      if (!panel) {
-        return;
-      }
-      panel.classList.toggle('display-nav-panel');
+      this.displayNavPanel = !this.displayNavPanel;
+    };
+    this.stopPropagationOfNavClick = (event) => {
+      event.stopPropagation();
     };
     this.setRoute = this.setRoute.bind(this);
     this.renderMenuItem = this.renderMenuItem.bind(this);
@@ -26,16 +26,27 @@ export class Navigation {
     this.route = location.hash.substr(1);
   }
   render() {
-    return (h("nav", { class: "nav-panel" },
-      h("a", { class: "nav-panel__responsive-menu", onClick: this.toggleMenu },
-        h("span", null),
-        h("span", null),
-        h("span", null),
-        h("span", null)),
-      h("header", { class: "panel-header" },
-        h("h1", null, this.renderHeader()),
-        h("kompendium-search", { index: this.index })),
-      this.renderChapters(this.menu)));
+    return [
+      h("div", { class: {
+          'nav-panel-scrim': true,
+          'display-nav-panel': this.displayNavPanel,
+        }, onClick: this.toggleMenu }),
+      h("nav", { class: {
+          'nav-panel': true,
+          'display-nav-panel': this.displayNavPanel,
+        }, onClick: this.stopPropagationOfNavClick },
+        h("a", { class: "nav-panel__responsive-menu", onClick: this.toggleMenu },
+          h("span", null),
+          h("span", null),
+          h("span", null),
+          h("span", null)),
+        h("header", { class: "panel-header" },
+          h("div", { class: "branding-and-mode" },
+            h("h1", null, this.renderHeader()),
+            h("kompendium-darkmode-switch", null)),
+          h("kompendium-search", { index: this.index })),
+        this.renderChapters(this.menu)),
+    ];
   }
   renderHeader() {
     let content = this.header;
@@ -61,15 +72,21 @@ export class Navigation {
       chapters: true,
       'panel-list': true,
     };
+    const chapters = item.children || [];
     const anchorClassList = {
       'panel-link': true,
       active: this.isRouteActive(item.path),
+      'has-children': !!chapters.length,
     };
-    const chapters = item.children || [];
-    const path = getAssetPath('../collection/assets/icons/arrow-right-s-line.svg');
+    const anchorAdditionalProps = {};
+    if (!chapters.length) {
+      anchorAdditionalProps.onClick = this.toggleMenu;
+    }
     return (h("li", { class: "panel-item" },
-      h("a", { class: anchorClassList, href: '#' + item.path },
-        h("img", { src: path }),
+      h("a", Object.assign({ class: anchorClassList, href: '#' + item.path }, anchorAdditionalProps),
+        h("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", width: "24", height: "24" },
+          h("path", { fill: "none", d: "M0 0h24v24H0z" }),
+          h("path", { id: "arrow", d: "M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z", fill: "currentColor" })),
         h("span", { class: "link-text" }, item.title)),
       h("ul", { class: classList }, chapters.map(this.renderMenuItem))));
   }
@@ -158,9 +175,9 @@ export class Navigation {
     }
   }; }
   static get states() { return {
-    "route": {}
+    "route": {},
+    "displayNavPanel": {}
   }; }
-  static get elementRef() { return "host"; }
 }
 // function hasContent(item: MenuItem) {
 //     return item.children?.length > 0;
