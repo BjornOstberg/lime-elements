e3cb2457f97a4162162b5d768b5ea674f2ea2f8d
lime-ci <617355+lime-ci@users.noreply.github.com>
2021-10-08 08:10:48 +0000

    chore(docs): create docs PR-1382

---
+++ versions/PR-1382/build/limel-example-tooltip.entry.js
@@ -0,0 +1,1 @@
+import{r as o,h as t}from"./index-c1a676a1.js";const l=class{constructor(t){o(this,t)}render(){return[t("limel-tooltip",{tooltip:"Search",hotkey:"alt+f"},t("limel-button",{slot:"trigger",icon:"search"}),";")]}};l.style=":host(limel-example-tooltip){--tooltip-body-background-color:rgb(var(--contrast-200))}";export{l as limel_example_tooltip}
